5e013c645dd509720834b6dcf6ff70e8e1d600c4
garfenter <garfenter@gmail.com>
2017-01-20 04:48:34 -0600

    Add oauth2 authentication

--- src/main/java/com/procesa/api/gateway/web/GatewayController.java
+++ src/main/java/com/procesa/api/gateway/web/GatewayController.java
@@ -1,13 +1,10 @@
 package com.procesa.api.gateway.web;
 
-import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.procesa.api.gateway.client.UserServiceClient;
-import com.procesa.api.gateway.domain.User;
+import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,13 +19,9 @@ public class GatewayController {
     private EurekaClient eurekaClient;
     @Autowired
     private UserServiceClient userServiceClient;
-    
-    @RequestMapping(value = "/services/{name}", method = RequestMethod.GET)
-    public InstanceInfo findUserByDbid(@PathVariable String name) {
-        User user = new User();
-        user.setName("Juancho");
-        user.setEmail("juancho@gmail.com");
-        userServiceClient.save(user);
-        return eurekaClient.getNextServerFromEureka(name, true);
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
     }
 }
